c4ae65623526d0154a732c162622c56206f509dc
liuanlin-mx <liuanlin-mx@qq.com>
2022-06-24 00:17:45 +0800

    ui实现添加网络到传输线列表

--- plugin/z_extractor.py
+++ plugin/z_extractor.py
@@ -22,7 +22,7 @@ class z_extractor_base ( wx.Dialog ):
 		sbSizer1 = wx.StaticBoxSizer( wx.StaticBox( self, wx.ID_ANY, u"Net Classes" ), wx.VERTICAL )
 
 		m_listBoxNetClassesChoices = []
-		self.m_listBoxNetClasses = wx.ListBox( sbSizer1.GetStaticBox(), wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, m_listBoxNetClassesChoices, wx.LB_HSCROLL|wx.LB_MULTIPLE|wx.LB_SORT )
+		self.m_listBoxNetClasses = wx.ListBox( sbSizer1.GetStaticBox(), wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, m_listBoxNetClassesChoices, wx.LB_MULTIPLE|wx.LB_SORT )
 		sbSizer1.Add( self.m_listBoxNetClasses, 1, wx.ALL|wx.EXPAND, 5 )
 
 		self.m_buttonRefresh = wx.Button( sbSizer1.GetStaticBox(), wx.ID_ANY, u"refresh", wx.DefaultPosition, wx.DefaultSize, 0 )
@@ -34,7 +34,7 @@ class z_extractor_base ( wx.Dialog ):
 		sbSizer7 = wx.StaticBoxSizer( wx.StaticBox( self, wx.ID_ANY, u"T-Line" ), wx.VERTICAL )
 
 		m_listBoxTLineChoices = []
-		self.m_listBoxTLine = wx.ListBox( sbSizer7.GetStaticBox(), wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, m_listBoxTLineChoices, 0 )
+		self.m_listBoxTLine = wx.ListBox( sbSizer7.GetStaticBox(), wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, m_listBoxTLineChoices, wx.LB_MULTIPLE )
 		sbSizer7.Add( self.m_listBoxTLine, 1, wx.ALL|wx.EXPAND, 5 )
 
 		self.m_buttonTLineAdd = wx.Button( sbSizer7.GetStaticBox(), wx.ID_ANY, u"add", wx.DefaultPosition, wx.DefaultSize, 0 )
@@ -49,7 +49,7 @@ class z_extractor_base ( wx.Dialog ):
 		sbSizer3 = wx.StaticBoxSizer( wx.StaticBox( self, wx.ID_ANY, u"Coupled" ), wx.VERTICAL )
 
 		m_listBoxCoupledChoices = []
-		self.m_listBoxCoupled = wx.ListBox( sbSizer3.GetStaticBox(), wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, m_listBoxCoupledChoices, 0 )
+		self.m_listBoxCoupled = wx.ListBox( sbSizer3.GetStaticBox(), wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, m_listBoxCoupledChoices, wx.LB_MULTIPLE )
 		sbSizer3.Add( self.m_listBoxCoupled, 1, wx.ALL|wx.EXPAND, 5 )
 
 		sbSizer71 = wx.StaticBoxSizer( wx.StaticBox( sbSizer3.GetStaticBox(), wx.ID_ANY, u"threshold" ), wx.VERTICAL )
@@ -61,8 +61,8 @@ class z_extractor_base ( wx.Dialog ):
 
 		bSizer6.Add( self.m_staticText2, 0, wx.ALL, 5 )
 
-		self.m_textCtrl2 = wx.TextCtrl( sbSizer71.GetStaticBox(), wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-		bSizer6.Add( self.m_textCtrl2, 0, 0, 5 )
+		self.m_textCtrlMinLen = wx.TextCtrl( sbSizer71.GetStaticBox(), wx.ID_ANY, u"0.5", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer6.Add( self.m_textCtrlMinLen, 0, 0, 5 )
 
 
 		sbSizer71.Add( bSizer6, 1, wx.EXPAND, 5 )
@@ -74,8 +74,8 @@ class z_extractor_base ( wx.Dialog ):
 
 		bSizer7.Add( self.m_staticText1, 0, wx.ALL, 5 )
 
-		self.m_textCtrl1 = wx.TextCtrl( sbSizer71.GetStaticBox(), wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-		bSizer7.Add( self.m_textCtrl1, 0, 0, 5 )
+		self.m_textCtrlMaxDist = wx.TextCtrl( sbSizer71.GetStaticBox(), wx.ID_ANY, u"2", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer7.Add( self.m_textCtrlMaxDist, 0, 0, 5 )
 
 
 		sbSizer71.Add( bSizer7, 1, wx.EXPAND, 5 )
@@ -100,7 +100,7 @@ class z_extractor_base ( wx.Dialog ):
 		sbSizer2 = wx.StaticBoxSizer( wx.StaticBox( self, wx.ID_ANY, u"Ref Net" ), wx.VERTICAL )
 
 		m_listBoxRefNetChoices = []
-		self.m_listBoxRefNet = wx.ListBox( sbSizer2.GetStaticBox(), wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, m_listBoxRefNetChoices, wx.LB_HSCROLL|wx.LB_MULTIPLE )
+		self.m_listBoxRefNet = wx.ListBox( sbSizer2.GetStaticBox(), wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, m_listBoxRefNetChoices, wx.LB_MULTIPLE )
 		sbSizer2.Add( self.m_listBoxRefNet, 1, wx.ALL|wx.EXPAND, 5 )
 
 		self.m_buttonRefNetAdd = wx.Button( sbSizer2.GetStaticBox(), wx.ID_ANY, u"add", wx.DefaultPosition, wx.DefaultSize, 0 )
@@ -115,7 +115,7 @@ class z_extractor_base ( wx.Dialog ):
 		sbSizer4 = wx.StaticBoxSizer( wx.StaticBox( self, wx.ID_ANY, u"cfg" ), wx.VERTICAL )
 
 		m_listBoxCfgChoices = []
-		self.m_listBoxCfg = wx.ListBox( sbSizer4.GetStaticBox(), wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, m_listBoxCfgChoices, 0 )
+		self.m_listBoxCfg = wx.ListBox( sbSizer4.GetStaticBox(), wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, m_listBoxCfgChoices, wx.LB_MULTIPLE )
 		sbSizer4.Add( self.m_listBoxCfg, 1, wx.ALL|wx.EXPAND, 5 )
 
 		self.m_buttonCfgAdd = wx.Button( sbSizer4.GetStaticBox(), wx.ID_ANY, u"add", wx.DefaultPosition, wx.DefaultSize, 0 )
@@ -137,7 +137,7 @@ class z_extractor_base ( wx.Dialog ):
 
 		m_radioBoxSpiceFmtChoices = [ u"ngspice(txl)", u"ngspice (ltra)" ]
 		self.m_radioBoxSpiceFmt = wx.RadioBox( sbSizer6.GetStaticBox(), wx.ID_ANY, u"Spice Format", wx.DefaultPosition, wx.DefaultSize, m_radioBoxSpiceFmtChoices, 1, wx.RA_SPECIFY_COLS )
-		self.m_radioBoxSpiceFmt.SetSelection( 2 )
+		self.m_radioBoxSpiceFmt.SetSelection( 0 )
 		sbSizer6.Add( self.m_radioBoxSpiceFmt, 0, wx.ALL, 5 )
 
 		bSizer8 = wx.BoxSizer( wx.VERTICAL )
@@ -163,40 +163,168 @@ class z_extractor_base ( wx.Dialog ):
 
 		self.Centre( wx.BOTH )
 
+		# Connect Events
+		self.m_buttonRefresh.Bind( wx.EVT_BUTTON, self.m_buttonRefreshOnButtonClick )
+		self.m_buttonTLineAdd.Bind( wx.EVT_BUTTON, self.m_buttonTLineAddOnButtonClick )
+		self.m_buttonTLineDel.Bind( wx.EVT_BUTTON, self.m_buttonTLineDelOnButtonClick )
+		self.m_buttonCoupledAdd.Bind( wx.EVT_BUTTON, self.m_buttonCoupledAddOnButtonClick )
+		self.m_buttonCoupledDel.Bind( wx.EVT_BUTTON, self.m_buttonCoupledDelOnButtonClick )
+		self.m_buttonRefNetAdd.Bind( wx.EVT_BUTTON, self.m_buttonRefNetAddOnButtonClick )
+		self.m_buttonRefNetDel.Bind( wx.EVT_BUTTON, self.m_buttonRefNetDelOnButtonClick )
+		self.m_buttonCfgAdd.Bind( wx.EVT_BUTTON, self.m_buttonCfgAddOnButtonClick )
+		self.m_buttonCfgDel.Bind( wx.EVT_BUTTON, self.m_buttonCfgDelOnButtonClick )
+		self.m_buttonCfgRename.Bind( wx.EVT_BUTTON, self.m_buttonCfgRenameOnButtonClick )
+		self.m_radioBoxSpiceFmt.Bind( wx.EVT_RADIOBOX, self.m_radioBoxSpiceFmtOnRadioBox )
+		self.m_buttonExtract.Bind( wx.EVT_BUTTON, self.m_buttonExtractOnButtonClick )
+		self.m_buttonSave.Bind( wx.EVT_BUTTON, self.m_buttonSaveOnButtonClick )
+
 	def __del__( self ):
 		pass
 
 
+	# Virtual event handlers, override them in your derived class
+	def m_buttonRefreshOnButtonClick( self, event ):
+		event.Skip()
 
+	def m_buttonTLineAddOnButtonClick( self, event ):
+		event.Skip()
+
+	def m_buttonTLineDelOnButtonClick( self, event ):
+		event.Skip()
+
+	def m_buttonCoupledAddOnButtonClick( self, event ):
+		event.Skip()
+
+	def m_buttonCoupledDelOnButtonClick( self, event ):
+		event.Skip()
+
+	def m_buttonRefNetAddOnButtonClick( self, event ):
+		event.Skip()
+
+	def m_buttonRefNetDelOnButtonClick( self, event ):
+		event.Skip()
+
+	def m_buttonCfgAddOnButtonClick( self, event ):
+		event.Skip()
+
+	def m_buttonCfgDelOnButtonClick( self, event ):
+		event.Skip()
+
+	def m_buttonCfgRenameOnButtonClick( self, event ):
+		event.Skip()
+
+	def m_radioBoxSpiceFmtOnRadioBox( self, event ):
+		event.Skip()
+
+	def m_buttonExtractOnButtonClick( self, event ):
+		event.Skip()
+
+	def m_buttonSaveOnButtonClick( self, event ):
+		event.Skip()
+
+
+
+
+class z_config_item():
+    def __init__(self):
+        self.tline = [];
+        
 
 class z_extractor_gui(z_extractor_base):
     def __init__(self):
         z_extractor_base.__init__(self, None)
-        board = pcbnew.GetBoard()
-        netcodes = board.GetNetsByNetcode()
-        net_dict = {}
+        self.cfg_dick = {}
+        self.cur_cfg = z_config_item()
+        self.cur_cfg_name = "test"
+        self.net_classes_list = []
+        self.board = pcbnew.GetBoard()
         
-        # list off all of the nets in the board.
+            
+        file_name = self.board.GetFileName()
+        self.path = os.path.split(file_name)[0]
+        file_name = os.path.split(file_name)[1]
+        name = file_name.rsplit('.',1)[0]
+        
+        self.update_net_classes_ui()
+        
+            
+    def update_net_classes_ui(self):
+        self.m_listBoxNetClasses.Clear()
+        self.net_classes_list = []
+        netcodes = self.board.GetNetsByNetcode()
         for netcode, net in netcodes.items():
-            print("netcode {}, name {}".format(netcode, net.GetNetname()))
-            net_dict[net.GetNetname()] = self.m_listBoxNetClasses.Append(net.GetNetname())
-            
-        fName = board.GetFileName()
-        path = os.path.split(fName)[0]
-        fName = os.path.split(fName)[1]
-        bomName = fName.rsplit('.',1)[0]
+            if net.GetNetname() not in self.net_classes_list:
+                self.net_classes_list.append(net.GetNetname())
+                self.m_listBoxNetClasses.Append(net.GetNetname())
         
-        tracks = board.GetTracks()
+        tracks = self.board.GetTracks()
         for track in tracks:
-            net = track.GetNet()
-            net.GetNetname()
             if track.IsSelected():
-                if net_dict.get(net.GetNetname()) != None:
-                    self.m_listBoxNetClasses.SetSelection(net_dict[net.GetNetname()])
-                #self.m_textCtrl1.AppendText(net.GetNetname() + "\n")
-            print(track)
+                net = track.GetNet()
+                self.m_textCtrlOutput.AppendText("{}\n".format(track))
+                if net.GetNetname() in self.net_classes_list:
+                    self.m_listBoxNetClasses.SetStringSelection(net.GetNetname())
             
+    
+    def update_tline_ui(self):
+        self.m_listBoxTLine.Clear()
+        for tline in self.cur_cfg.tline:
+            self.m_listBoxTLine.Append(tline)
             
+        
+    # Virtual event handlers, override them in your derived class
+    def m_buttonRefreshOnButtonClick( self, event ):
+        self.update_net_classes_ui()
+
+    def m_buttonTLineAddOnButtonClick( self, event ):
+        selected = self.m_listBoxNetClasses.GetSelections()
+        for n in selected:
+            str = self.m_listBoxNetClasses.GetString(n)
+            if str not in self.cur_cfg.tline:
+                self.cur_cfg.tline.append(str)
+                self.m_listBoxTLine.Append(str)
+        
+            
+    def m_buttonTLineDelOnButtonClick( self, event ):
+        selected = self.m_listBoxTLine.GetSelections()
+        size = len(selected)
+        while size > 0:
+            size = size - 1
+            str = self.m_listBoxTLine.GetString(selected[size])
+            self.m_listBoxTLine.Delete(selected[size])
+            self.cur_cfg.tline.remove(str)
+            
+
+    def m_buttonCoupledAddOnButtonClick( self, event ):
+        event.Skip()
+
+    def m_buttonCoupledDelOnButtonClick( self, event ):
+        event.Skip()
+
+    def m_buttonRefNetAddOnButtonClick( self, event ):
+        event.Skip()
+
+    def m_buttonRefNetDelOnButtonClick( self, event ):
+        event.Skip()
+
+    def m_buttonCfgAddOnButtonClick( self, event ):
+        event.Skip()
+
+    def m_buttonCfgDelOnButtonClick( self, event ):
+        event.Skip()
+
+    def m_buttonCfgRenameOnButtonClick( self, event ):
+        event.Skip()
+
+    def m_radioBoxSpiceFmtOnRadioBox( self, event ):
+        event.Skip()
+
+    def m_buttonExtractOnButtonClick( self, event ):
+        event.Skip()
+
+    def m_buttonSaveOnButtonClick( self, event ):
+        event.Skip()
+        
 class z_extractor( pcbnew.ActionPlugin ):
     def defaults( self ):
         self.name = "Impedance Extractor"
